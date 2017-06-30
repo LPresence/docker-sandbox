fe00f62d37561aa3c7bd1146b9166052d389091f
buknon <tristan.miche@soprasteria.com>
2017-06-30 10:20:08 +0200

    add opa as service

--- enable-opa.sh
+++ enable-opa.sh
@@ -3,5 +3,16 @@ curl -L https://github.com/open-policy-agent/opa-docker-authz/releases/download/
 chmod u+x opa-docker-authz
 sudo echo DOCKER_OPTS=\"-D --authorization-plugin=opa-docker-authz --icc=false --storage-driver=devicemapper --tls=false -H unix:///var/run/docker.sock -H tcp://$ip:2375\" >> /etc/default/docker
 sudo service docker restart
-sudo ./opa-docker-authz -opa-url=http://192.168.99.107:8181/v1 &
+sudo tee /etc/init/opa.conf<<OPA
+start on (filesystem and net-device-up IFACE!=lo)
+stop on runlevel [!2345]
+
+respawn
+
+exec /home/vagrant/opa-docker-authz -opa-url=http://192.168.99.107:8181/v1
+OPA
+
+sudo start opa
+#sudo ./opa-docker-authz -opa-url=http://192.168.99.107:8181/v1 &
+
 
