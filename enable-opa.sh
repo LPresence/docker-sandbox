cd29cbf08332fe28b804ef2179907715cea942de
gilleslabs <gilles.labs.tosi@gmail.com>
2017-07-01 16:43:56 +0200

    fixed docler-compose error

--- enable-opa.sh
+++ enable-opa.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo apt-get install curl -y
 curl -L https://github.com/open-policy-agent/opa-docker-authz/releases/download/v0.1.5/opa-docker-authz_linux_amd64 > opa-docker-authz
 chmod u+x opa-docker-authz
 sudo echo DOCKER_OPTS=\"-D --authorization-plugin=opa-docker-authz --icc=false --storage-driver=devicemapper --tls=false -H unix:///var/run/docker.sock -H tcp://$ip:2375\" >> /etc/default/docker
