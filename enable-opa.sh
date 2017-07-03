4ed989850defdf2754ba41cf4212e3ee0c4d6b26
gilleslabs <gilles.labs.tosi@gmail.com>
2017-07-03 19:04:55 +0200

    update

--- enable-opa.sh
+++ enable-opa.sh
@@ -2,6 +2,7 @@
 sudo apt-get install curl -y
 curl -L https://github.com/open-policy-agent/opa-docker-authz/releases/download/v0.1.5/opa-docker-authz_linux_amd64 > opa-docker-authz
 chmod u+x opa-docker-authz
+ip=$(ifconfig eth1 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 sudo echo DOCKER_OPTS=\"-D --authorization-plugin=opa-docker-authz --icc=false --storage-driver=devicemapper --tls=false -H unix:///var/run/docker.sock -H tcp://$ip:2375\" >> /etc/default/docker
 sudo service docker restart
 sudo tee /etc/init/opa.conf<<OPA
